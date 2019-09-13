6d798d3d80b2f6a9a703a66a09131022c70a06f8
Augustin Bengolea <Georges.Bengolea@etu.univ-grenoble-alpes.fr>
2019-09-13 11:46:08 +0200

    Added button entities for admin.

--- src/main/webapp/app/layouts/navbar/navbar.route.ts
+++ src/main/webapp/app/layouts/navbar/navbar.route.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 
-import { NavbarComponent } from './navbar.component';
+// import { NavbarComponent } from './navbar.component';
 import { ToolbarComponent } from 'app/sabike/components/toolbar/toolbar.component';
 
 export const navbarRoute: Route = {
