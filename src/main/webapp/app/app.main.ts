52f411d2917dd4186cb41d427619eb33a10d8c04
Augustin Bengolea <Georges.Bengolea@etu.univ-grenoble-alpes.fr>
2019-09-19 14:32:56 +0200

    Material register. Added backdrop to Log in dialog

--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ProdConfig } from './blocks/config/prod.config';
+import 'hammerjs';
 import { SabikeAppModule } from './app.module';
 
 ProdConfig();
