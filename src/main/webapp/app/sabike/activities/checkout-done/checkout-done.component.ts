109989f55f9fa6e181d613b4e504d8eb6d7bfd31
Augustin Bengolea <Georges.Bengolea@etu.univ-grenoble-alpes.fr>
2019-09-24 22:23:38 +0200

    Checkout done

---
+++ src/main/webapp/app/sabike/activities/checkout-done/checkout-done.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-checkout-done',
+  templateUrl: './checkout-done.component.html',
+  styleUrls: ['./checkout-done.component.scss']
+})
+export class CheckoutDoneComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
