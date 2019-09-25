568c15296abae4ed8bb08f3677cc548cfdc71218
Augustin Bengolea <Georges.Bengolea@etu.univ-grenoble-alpes.fr>
2019-09-26 01:01:56 +0200

    History

---
+++ src/main/webapp/app/sabike/activities/history/history-list/history-list-item/history-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-history-list-item',
+  templateUrl: './history-list-item.component.html',
+  styleUrls: ['./history-list-item.component.scss']
+})
+export class HistoryListItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
